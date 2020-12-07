1a2e057a0b9450b99b57c2ef96e9992a9834a2f8
James Bian <james.bian23@ycdsbk12.ca>
2020-12-07 18:15:34 +0000

    I added a title for both problems, and I finished problem 2

--- problem1.py
+++ problem1.py
@@ -1,3 +1,15 @@
+'''
+--------------------------
+Name: problem1.py
+Purpose: Calculate the temperature of the weather in Fahrenheit based on Celsius
+
+Author: James Bian
+
+Created: date 07/12/2020
+--------------------------
+'''
+
 celsius = float(input("What degrees celsius does it say it is?: "))
-print("The temperature in fahrenheit is: ", (celsius * 9) / 5 + 32 )
+print("The temperature in fahrenheit would be: ", (celsius * 9) / 5 + 32 )
+
 
--- problem2.py
+++ problem2.py
@@ -1,1 +1,20 @@
+'''
+--------------------------
+Name: problem2.py
+Purpose: Calculates the number of chickens shared between students and Mr. Fabroa
+
+Author: James Bian
+
+Created: date 07/12/2020
+--------------------------
+'''
+
+
+
+students = float(input('How many students are in you class, Mr Fabroa?: '))
+chicken = int(input("how many pieces of chicken from Popeyes did you get?: "))
+divide =  chicken / students
+fabroa = chicken % students
+print ("Each student gets this many chickens: ", divide)
+print ("Mr. Fabroa gets: ", fabroa)
 
